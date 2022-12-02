b55e8ddb22150df49bd2109b4cbe2e6b13d91473
saliwan <saliwanm@gmail.com>
2022-12-02 15:38:11 +0200

    four commit

--- app/models/utils.py
+++ app/models/utils.py
@@ -15,6 +15,21 @@ class ModelMixin(object):
 
 class MailSend(object):
 
+    # def send_email(message):
+    #     sender = "saliwanm@gmail.com"
+    #     password = "password"
+    #
+    #     server = smtplib.SMTP("smtp.gmail.com", 587)
+    #     server.starttls()
+    #
+    #     try:
+    #         server.login(sender, password)
+    #         server.sendmail(sender, sender, message)
+    #
+    #         return "The message was sent successfully!"
+    #     except Exception as _ex:
+    #         return f"{_ex}\nCheck your login or password please!"
+
     def send_email(message):
         # server = 'smtp.gmail.com'
         # user = 'saliwanm@gmail.com'
